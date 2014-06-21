f7ae40f13e65793bdf58f6060725ad6d141eaffe
John Ryan and Mariana Lenetis <pair+john+mariana@pivotallabs.com>
2014-06-20 18:06:03 -0700

    did some more work

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  raise TriangleError, "Negative side" if a <= 0 or b <= 0 or c <= 0
+  raise TriangleError, "One Side Too Long" if a+b <= c or
+                                              b+c <= a or
+                                              a+c <= b
+  equal_sides = 0
+  equal_sides += 1 if a == b
+  equal_sides += 1 if a == c
+  equal_sides += 1 if b == c
+  case equal_sides
+  when 0
+    return :scalene
+  when 1
+    return :isosceles
+  when 3
+    return :equilateral
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
